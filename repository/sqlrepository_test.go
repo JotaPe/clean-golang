f063c5b6dadf7d029ec86f8c8ec92056a1518677
JotaPe <jotape@protonmail.com>
2021-02-19 18:47:06 -0300

    using sqlite for testing repository

--- repository/sqlrepository_test.go
+++ repository/sqlrepository_test.go
@@ -1,114 +1,40 @@
 package repository
 
-/*
 import (
 	"clean-arch/entity"
 
-	"database/sql"
+	//"database/sql"
 	"testing"
 
 	"github.com/gofrs/uuid"
-	"gopkg.in/DATA-DOG/go-sqlmock.v1"
-	"gorm.io/driver/postgres"
+	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
-// Mock struct
-type GormSuite struct {
-	db   *gorm.DB
-	mock sqlmock.Sqlmock
+var db *gorm.DB
+
+func TestMain(t *testing.T) {
+	var err error
+	db, err = gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		t.Fatal(err)
+	}
 }
 
 func TestGenerateEveryRun(t *testing.T) {
-	suite := &GormSuite{}
-	var (
-		db  *sql.DB
-		err error
-	)
-
-	db, suite.mock, err = sqlmock.New()
-	if err != nil {
-		t.Error(err)
-	}
-	if db == nil {
-		t.Error("Mock db is null")
-	}
-
-	dialector := postgres.New(postgres.Config{
-		DSN:                  "sqlmock_db_0",
-		DriverName:           "postgres",
-		Conn:                 db,
-		PreferSimpleProtocol: true,
-	})
-	suite.db, _ = gorm.Open(dialector, &gorm.Config{})
-	if err != nil {
-		t.Error(err)
-	}
-	suite.mock.MatchExpectationsInOrder(false)
-	suite.mock.ExpectBegin()
-	t.Log(suite.db)
-
-	suite.mock.ExpectCommit()
-	suite.db.AutoMigrate(&Post{})
-
-	defer db.Close()
-
-	postRepository := NewSqlRepository(suite.db)
+	postRepository := NewSqlRepository(db)
 
 	generateId, _ := uuid.NewV4()
-	suite.mock.ExpectCommit()
 	generate, err := postRepository.Save(&entity.Post{ID: generateId.String(), Title: "Test Title", Text: "Test Text"})
 	if err != nil {
 		t.Fatal(err)
 	}
-	suite.mock.ExpectationsWereMet()
 
 	t.Logf("Generated post: %v", generate)
 }
 
 func TestFindAll(t *testing.T) {
-	suite := &GormSuite{}
-	var (
-		db  *sql.DB
-		err error
-	)
-
-	db, suite.mock, err = sqlmock.New()
-	if err != nil {
-		t.Error(err)
-	}
-	if db == nil {
-		t.Error("Mock db is null")
-	}
-	if suite.mock == nil {
-		t.Error("sqlmock is null")
-	}
-
-	dialector := postgres.New(postgres.Config{
-		DSN:                  "sqlmock_db_0",
-		DriverName:           "postgres",
-		Conn:                 db,
-		PreferSimpleProtocol: true,
-	})
-
-	suite.db, err = gorm.Open(dialector, &gorm.Config{})
-	if err != nil {
-		t.Error(err)
-	}
-	t.Log(suite.db)
-
-	suite.db.AutoMigrate(&Post{})
-
-	defer db.Close()
-
-	postRepository := NewSqlRepository(suite.db)
-	if err != nil {
-		t.Error(err)
-	}
-
-	generateID, _ := uuid.NewV4()
-	generate, err := postRepository.Save(&entity.Post{ID: generateID.String(), Title: "Test Title", Text: "Test Text"})
-	t.Logf("Generated for FINDALL: %v", generate)
+	postRepository := NewSqlRepository(db)
 
 	posts, err := postRepository.FindAll()
 	if err != nil {
@@ -118,4 +44,3 @@ func TestFindAll(t *testing.T) {
 		t.Log(i, s)
 	}
 }
-*/
